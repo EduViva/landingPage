e64fc1f60e7280c51c9072d20cd8210e673f8566
EduViva <miissjackson72@gmail.com>
2020-04-27 21:07:45 -0300

    tiny-balls, adicionada big feature

--- Imagem-bg/funcs.js
+++ Imagem-bg/funcs.js
@@ -1,8 +1,19 @@
 $(function(){
 
-    $(".chevron").on('click', () => window.scrollTo({
-        top: 620,
-        behavior:"smooth",
-    }));
+  $(".chevron").on('click', () => window.scrollTo({
+      top: 620,
+      behavior:"smooth",
+  }));
+
+  $(window).scroll(function(e){
+    parallax();
+  });
+
+  function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.tiny-ball').css('top',-(scrolled*0.36)+'px');
+  }
 
 })
+
+
